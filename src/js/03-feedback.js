9934caf2e311d589e300bbe483e0de0743f8ffbb
alena <evstafienko_a@ukr.net>
2022-11-22 17:43:19 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,54 @@
-// HTML містить розмітку форми.Напиши скрипт, який буде зберігати значення полів у 
-// локальне сховище, коли користувач щось друкує.
-
-// < class="feedback-form" autocomplete="off">
-//  <input type="email" name="email" autofocus />
-//  <textarea name="message" rows="8"></textarea>
-//  <button type="submit">Submit</button>
 
 //   1) Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями 
-// email і message, у яких зберігай поточні значення полів форми.Нехай ключем для сховища 
-// буде рядок "feedback-form-state".
-// 2) Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані,
-//   заповнюй ними поля форми.В іншому випадку поля повинні бути порожніми.
+// email і message, у яких зберігай поточні значення полів форми.
 // 3) Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з 
 // полями email, message та їхніми поточними значеннями.
-// 4) Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. 
-// Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
 
 //  перевіряти всі значення, які ви читаєте з localStorage і 
 // записуєте в поля форми.Якщо там пусто, вам у поля запишеться undefined.При сабміті форми
 //  не забувайте чистити обʼєкт в якому зберігаєте значення з полів форми, щоб інформація не 
 //  тягнулась в наступні сабміти.Форма має відправлятись при заповнених 2 - х полях форми
 
+import throttle from "lodash.throttle";
+
 const refs = {
   formEl: document.querySelector('.feedback-form'),
   textareaEl : document.querySelector('.feedback-form textarea'),
 }
+const STORAGE_KEY = 'feedback-form-state';
+// зберігаємо імейл та повідомлення в обєкт
+let formData = {};
+refs.formEl.addEventListener('input', e =>{
+  formData[e.target.name] = e.target.value;
+  console.log(formData);
+});
+
+  refs.formEl.addEventListener('submit', onFormSubmit);
+  refs.textareaEl.addEventListener('input', throttle(onTextareaInput, 500) );
+ 
+function onTextareaInput(e){
+  // отримуємо значення
+  const message = e.target.value;
+  // зберігаємо його в сховище
+  localStorage.setItem(STORAGE_KEY, message )
   
-console.log(ra);
+}
+
+function onFormSubmit(e){
+  // Х поведінку по замовчуванню
+e.preventDefault;
+// Забираємо повідомлення із сховища і чистимо форму, reset() для очистки форм
+e.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY); 
+}
+
+populateTextarea();
+function populateTextarea(){
+  // при перезагрузці форми зберігається повідомлення, якщо воно не було відправлене
+const savadMessage = localStorage.getItem(STORAGE_KEY);
+  // якщо вже є відгук, то його записуємо в localStorage
+if (savadMessage){
+   refs.textareaEl.value = savadMessage;
+}
+}
+// console.log();
