6142a9b928370fb5d5fc7a14d0714c4278e8dff5
alena <evstafienko_a@ukr.net>
2022-11-22 15:45:10 +0200

    3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,48 @@
-// Add imports above this line
+// 1) Додай бібліотеку SimpleLightbox як залежність проекту, використовуючи
+// npm(посилання на CDN з твоєї минулої роботи більше не потрібне).
+// 2) Використовуй свій JavaScript код з попередньої домашньої роботи, але виконай рефакторинг
+//  з урахуванням того, що бібліотека була встановлена через npm(синтаксис import /export).
+// 3) Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт,
+//    крім того, що описаний в документації.
+
+// Описаний в документації
+// import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.addEventListener("click", onClickCreateModalWindow);
+
+const itemEl = galleryItems
+  .map(
+    (image) => `
+<a class="gallery__item" href =${image.original} ><img class="gallery__image" 
+  src = ${image.preview} alt = ${image.description}/></a>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", itemEl);
+
+function onClickCreateModalWindow(e) {
+  // забороняємо стандартні дії
+  e.preventDefault();
+  // вказую, що відкривати лише img
+  const isIMGEl = e.target.nodeName;
+  if (isIMGEl !== "IMG") {
+    return;
+  }
+}
+  // підключаємо бібліотеку SimpleLightbox
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionsDelay: 250,
+  });
+console.log(galleryItems);
+
+
 
 console.log(galleryItems);
---
+++ src/js/try.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("#message-form");
+const output = document.querySelector("#output");
+// const LOCALSTORAGE_KEY = "goit-example-message";
+
+// updateOutput();
+// form.addEventListener("submit", saveMessage);
+
+// function saveMessage(evt) {
+//   // при роботі з формами не перезавантажує сторінку
+//   evt.preventDefault();
+//   // оновлює дані в LOCALSTORAGE_KEY що вводимо в інпут
+//   localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value);
+//   updateOutput();
+//   // метод reset() очищає всі значення елементів форми
+//   form.reset();
+ 
+// }
+
+// function updateOutput() {
+//   output.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
+// }
+
+
+
