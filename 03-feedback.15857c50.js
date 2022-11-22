38f5c2846ead719a16e907b6dd7590968b65f2df
AlenaMushko <108529764+AlenaMushko@users.noreply.github.com>
2022-11-22 15:43:53 +0000

    Deploying to gh-pages from @ AlenaMushko/goit-js-hw-08@9934caf2e311d589e300bbe483e0de0743f8ffbb 🚀

--- 03-feedback.15857c50.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form"),document.querySelector(".feedback-form textarea");console.log(ra);
-//# sourceMappingURL=03-feedback.15857c50.js.map
--- 03-feedback.b8b61a56.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".feedback-form"),document.querySelector(".feedback-form textarea");console.log(ra)}();
-//# sourceMappingURL=03-feedback.b8b61a56.js.map
