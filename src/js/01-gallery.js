403d91c2ed3e1e15b1c1dda7009515b74a3038c3
alena <evstafienko_a@ukr.net>
2022-11-24 17:16:37 +0200

    all

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 
-// import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 import galleryCardTpl from '../templates/gallery-card.hbs';
